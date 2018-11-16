c9ecd2fd92cc1214a23ce198bb0e50480da626fa
AC Craft <accraft4@gmail.com>
2018-11-16 01:58:02 -0500

    removed unnecessary series, added TTM option, now show full name in hoverlabel

--- net_generation_by_source.py
+++ net_generation_by_source.py
@@ -1,11 +1,12 @@
 #! python
 import requests 
 import pprint
+import pandas as pd
 import plotly.graph_objs as go
 import plotly.plotly as py
 import plotly 
+import sys
 from datetime import datetime
-
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -14,40 +15,51 @@ eia_api_key = "a6b68dab9928c665bccd9a853c544092"
 def get_url (url):
 	r = requests.get(url)
 	return r.json()
-
 	
 def get_childseries (category_num):
 	url = "http://api.eia.gov/category/?api_key=" + eia_api_key + "&category_id=" + str(category_num)
 	return_json = get_url(url)
-	return return_json['category']['childcategories'] 
+	return {'title':return_json['category']['name']
+			,'data':return_json['category']['childcategories']}
 #sample call below. I know 3 is the category number for net generation by source
 #get_childseries(3) 
 
 def get_seriesid_by_geo_time(series,geography='United States',timeperiod='monthly'):
 	series_json = get_url("http://api.eia.gov/category/?api_key=" + eia_api_key + "&category_id=" + str(series))
-	return [x for x in series_json['category']["childseries"] if geography in x['name'] and timeperiod in x['name']][0]['series_id']
-
+	try: 
+		return [x for x in series_json['category']["childseries"] if geography in x['name'] and timeperiod in x['name']][0]['series_id']
+	except IndexError:
+		return "Does not exist"
+#returns the category ID, which is used to actually pull the data. Sample call below:
+#get_seriesid_by_geo_time(4,"Oregon","quarterly")
+		
 def get_series_by_geo_time(series,geography='United States',timeperiod='monthly'):
 	series_id = get_seriesid_by_geo_time(series,geography,timeperiod)
 	series_id_results = get_url("http://api.eia.gov/series/?api_key=" + eia_api_key + "&series_id=" + series_id)
 	return series_id_results['series'][0]
-
 #sample call, returns the series ID for coal. 
 #coal_series_test = get_series_by_geo_time(4,'United States','monthly')
 
-def return_childseries_tograph(toloop):
-	all_series = []
-	for series in toloop:
-		series_results = get_series_by_geo_time(series['category_id'])
-		all_series.append(series_results)
-	return all_series
 	
-def prep_scatter(indata,title):
-	return go.Scatter( x = [datetime.strptime(d[0],'%Y%m') for d in indata['data']]
-		, y = [d[1] for d in indata['data']]
+def prep_scatter(in_x,indata,title,visible=True):
+	return go.Scatter( x = [datetime.strptime(d,'%Y%m') for d in in_x]
+		, y = indata
 		, mode='lines+markers'
-		, name=title) 
+		, name=title
+		, visible=visible
+		, hoverlabel = dict(namelength = -1)) 
+#function to prepare the scatter plot 
 
+def prep_source_df(geo_series_by_geo_time_result,name):
+	date_list = []
+	value_list = []
+	
+	for rec in geo_series_by_geo_time_result['data']:
+		date_list.append(rec[0])
+		value_list.append(rec[1])
+	df_results = pd.DataFrame(data={'date':date_list,name:value_list})
+	return df_results	
+#gets source data and converts it into a two column dataframe: date, value. 
 	
 #takes as set of results from get_childseries, loops through them, and then appends the resulting data to a list
 #two_cat_sample = [x for x in get_childseries(3) if x['category_id'] < 8]
@@ -59,23 +71,54 @@ def main():
 	pp.pprint(all_resultlist)
 	#relevent_resultlist = [x for x in all_resultlist if x['category_id'] < 100 ]
 	#data_to_graph = return_childseries_tograph(relevent_resultlist)
-	coal_data = get_series_by_geo_time(4,'United States','monthly')
-	wind_data = get_series_by_geo_time(14,'United States','monthly')
-	solar_data = get_series_by_geo_time(15,'United States','monthly')
 	
-	trace_coal = prep_scatter(solar_data,'Utility Scale Solar')
-	trace_wind = prep_scatter(wind_data,'Wind')
-	trace_solar = prep_scatter(coal_data,'Coal')
+	all_resultlist_tograph = [x for x in all_resultlist['data'] if (len(str(x['category_id'])) < 3 or x['name'] == 'All fuels') and x['name'] != 'Other renewables (total)' ]
+	#create shell of dataframe to join source data to. 
+	df_net_gen_results = pd.DataFrame(columns={'date'})
+	df_net_gen_results_ttm = pd.DataFrame(columns={'date'})
+	for source in all_resultlist_tograph:
+		df_tojoin = prep_source_df(get_series_by_geo_time(source['category_id'],'United States','monthly'),source['name'])
+		df_net_gen_results = df_net_gen_results.merge(df_tojoin,how='outer',on='date')
+		
+		df_tojoin_ttm = pd.DataFrame({'date':df_tojoin['date'] , source['name']:df_tojoin[source['name']][::-1].rolling(window=12).mean()[::-1]})
+		df_net_gen_results_ttm = df_net_gen_results_ttm.merge(df_tojoin_ttm,how='outer',on='date')
 	
-	data = [trace_solar,trace_wind,trace_coal]
+	chart_data = []
+	non_ttm_visible = []
+	for column in df_net_gen_results:
+		if column != 'date' and column != 'All fuels':
+			chart_data.append(prep_scatter(df_net_gen_results['date'],df_net_gen_results[column],column))	
+			non_ttm_visible.append(True)
+	
+	for column in df_net_gen_results_ttm:
+		if column != 'date' and column != 'All fuels':
+			chart_data.append(prep_scatter(df_net_gen_results_ttm['date'],df_net_gen_results_ttm[column],column,visible=False))	
+			non_ttm_visible.append(False)
+	
+	ttm_visible = [not i for i in non_ttm_visible]
+	
+	updatemenus = list([
+		dict(active=-1,
+			 buttons=list([   
+				dict(label = 'Actual',
+					 method = 'update',
+					 args = [{'visible': non_ttm_visible}]),
+				dict(label = 'TTM',
+					 method = 'update',
+					 args = [{'visible': ttm_visible}])
+				])
+			)])
+
 	
 	layout = dict(title = 'Net Generation By Source',
               xaxis = dict(title = 'Month'),
               yaxis = dict(title = 'thousand megawatthours'),
-              )
+			  hovermode = 'closest',
+              updatemenus = updatemenus
+			  )
 	
 	plotly.offline.plot({
-			'data': data,
+			'data': chart_data,
 			"layout": layout}
 		,filename='Net Gen by Source - line chart'
 		)
@@ -84,10 +127,6 @@ if __name__ == '__main__':
 	main()
 
 '''	
-all_resultlist = get_childseries(3)
-relevent_resultlist = [x for x in all_resultlist if x['category_id'] < 100 ]
-data_to_graph = return_childseries_tograph(relevent_resultlist)
-	
 #get list of all localities
 #coal_test = get_url("http://api.eia.gov/category/?api_key=a6b68dab9928c665bccd9a853c544092&category_id=4")
 #set([x['name'].split(":")[2].strip() for x in coal_test['category']["childseries"]])
